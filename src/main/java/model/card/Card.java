627e68b34272bb9d7a5507ad31c4a46918070b86
soheil <soheilsayahvarg@gmail.com>
2024-05-04 14:06:54 +0330

    edit card model and add No. of cards in game

--- src/main/java/model/card/Card.java
+++ src/main/java/model/card/Card.java
@@ -5,47 +5,52 @@ import enums.cardsinformation.*;
 public class Card {
     private final String NAME;
     private final Type TYPE;
+    private final int NO_OF_CARDS_IN_GAME;
     private int power;
-    private final Ability ABILITY;  // Assuming you have an Ability enum similar to CardType and CardPower
-    private final Faction FACTION;  // Adding Faction as a property of Card
+    private final Ability ABILITY;
+    private final Faction FACTION;
     private final Description DESCRIPTION;
 
-    public Card(String name, Type type, int power, Ability ability, Faction faction, Description description) {
+    public Card(String name, Type type, int noOfCardsInGame, int power, Ability ability,
+                Faction faction, Description description) {
         this.NAME = name;
         this.TYPE = type;
+        this.NO_OF_CARDS_IN_GAME = noOfCardsInGame;
         this.power = power;
         this.ABILITY = ability;
+        this.FACTION = faction;
         this.DESCRIPTION = description;
-        this.FACTION = faction;  // Initialize faction
     }
 
-    // Getters
-    public String getName() {
+    public String getNAME() {
         return NAME;
     }
 
-    public Type getType() {
+    public Type getTYPE() {
         return TYPE;
+    }
+
+    public int getNO_OF_CARDS_IN_GAME() {
+        return NO_OF_CARDS_IN_GAME;
+    }
+
+    public Ability getABILITY() {
+        return ABILITY;
+    }
+
+    public Faction getFACTION() {
+        return FACTION;
+    }
+
+    public Description getDESCRIPTION() {
+        return DESCRIPTION;
     }
 
     public int getPower() {
         return power;
     }
 
-    public Ability getAbility() {
-        return ABILITY;
-    }
-
-    public Faction getFaction() {
-        return FACTION;
-    }
-
-    public Description getDescription() {
-        return DESCRIPTION;
-    }
-
     public void setPower(int power) {
         this.power = power;
     }
-
 }
