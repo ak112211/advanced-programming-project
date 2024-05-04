3cd3b95246ad395a36829e720a4c696e0466c9ad
soheil <soheilsayahvarg@gmail.com>
2024-05-04 14:56:17 +0330

    edit leaders

--- src/main/java/enums/leaders/MonstersLeaders.java
+++ src/main/java/enums/leaders/MonstersLeaders.java
@@ -1,5 +1,18 @@
 package enums.leaders;
 
-public enum MonstersLeaders {
+import enums.cardsinformation.Faction;
+import model.card.Leader;
 
+public enum MonstersLeaders {
+    BRINGER_OF_DEATH("Bringer of Death");
+
+    private final String NAME;
+
+    MonstersLeaders(String name) {
+        this.NAME = name;
+    }
+
+    public Leader getLeader() {
+        return new Leader(NAME, Faction.MONSTER);
+    }
 }
--- src/main/java/model/card/Leader.java
+++ src/main/java/model/card/Leader.java
@@ -5,9 +5,26 @@ import enums.cardsinformation.Faction;
 public class Leader {
     private final String NAME;
     private final Faction FACTION;
+    private int numberOfAction = 1;
 
     public Leader(String name, Faction faction) {
         this.NAME = name;
         this.FACTION = faction;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public Faction getFaction() {
+        return FACTION;
+    }
+
+    public int getNumberOfAction() {
+        return numberOfAction;
+    }
+
+    public void setNumberOfAction(int numberOfAction) {
+        this.numberOfAction = numberOfAction;
     }
 }
