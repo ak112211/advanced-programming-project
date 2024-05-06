1344b21a4fb0e2177195a6478d7ec4490b6872b4
mahdishb <mahdishb2006@gmail.com>
2024-05-06 11:20:07 +0330

    finshed scorch and spy

--- src/main/java/model/abilities/instantaneousabilities/Scorch.java
+++ src/main/java/model/abilities/instantaneousabilities/Scorch.java
@@ -1,13 +1,36 @@
 package model.abilities.instantaneousabilities;
 
 import enums.Row;
+import enums.cardsinformation.Type;
 import model.Game;
 import model.card.Card;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Scorch extends InstantaneousAbility {
-    public Scorch() {
-
+    Type type;
+    /**
+    @param type selects which row should it remove, type=null means it can affect every rows
+     */
+    public Scorch(Type type) {
+        this.type = type;
+    }
+    private List<Card> getCardsInRow(ArrayList<Card> inGameCards) {
+        if (type == null){
+            return inGameCards;
+        }
+        Row row = type.getRow(getCard().getRow().isPlayer1());
+        return inGameCards.stream().filter(card -> card.getRow()==row).toList();
+    }
+    public void affect(Game game) {
+        List<Card> cards = getCardsInRow(game.getInGameCards());
+        int maxPower = cards.stream().mapToInt(Card::getPower).max().orElse(0); // if it's empty maxPower value isn't important
+        cards.stream().filter(card -> card.getPower() == maxPower)
+                .forEach(game::moveCardToGraveyard);
     }
 }
--- src/main/java/model/abilities/instantaneousabilities/Spy.java
+++ src/main/java/model/abilities/instantaneousabilities/Spy.java
@@ -1,5 +1,19 @@
 package model.abilities.instantaneousabilities;
 
-public class Spy extends InstantaneousAbility {
+import model.Game;
+import model.card.Card;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Spy extends InstantaneousAbility {
+    public void affect(Game game) {
+        ArrayList<Card> deck = !getCard().getRow().isPlayer1() ? game.getPlayer1Deck() : game.getPlayer2Deck();
+        ArrayList<Card> inHandCards = !getCard().getRow().isPlayer1() ? game.getPlayer1InHandCards() : game.getPlayer2InHandCards();
+        Random rand = new Random();
+        for (int i = 0; i < 2; i++) {
+            Card card = deck.get(rand.nextInt(deck.size()));
+            game.moveCard(card, deck, inHandCards);
+        }
+    }
 }
