2275cf0fb917e5538de4d5ac8851860668e31e4e
mahdishb <mahdishb2006@gmail.com>
2024-05-06 12:24:29 +0330

    updated Berserker

--- src/main/java/model/abilities/Berserker.java
+++ src/main/java/model/abilities/Berserker.java
@@ -1,18 +1,12 @@
 package model.abilities;
 
-import enums.Row;
-import model.App;
-import model.Game;
-import model.abilities.Ability;
-import model.abilities.instantaneousabilities.InstantaneousAbility;
 import model.card.Card;
-
 import java.util.ArrayList;
 
 public class Berserker extends Ability {
-    private Card newCard;
-    public Berserker (){
-
+    private final Card newCard;
+    public Berserker (Card newCard){
+        this.newCard = newCard;
     }
     public void affect(ArrayList<Card> inGameCards){
         int i = inGameCards.indexOf(getCard());
