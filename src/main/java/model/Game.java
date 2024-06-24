429fa82d20bf74b792cf50ef11a084b2ab45aa1a
MahdiShb <mahdishb2006@gmail.com>
2024-06-24 20:08:46 +0330

    changed chooseCard method inputs

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -158,7 +158,7 @@ public class Game {
 
     }
 
-    public Card chooseCard(ArrayList<Card> cards) {
+    public Card chooseCard(ArrayList<Card> cards, boolean onlyAffectables) {
         return null; // TODO
     }
 
