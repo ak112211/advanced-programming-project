346dc1ae08afca5bf3517ac3a8d358779190a1f6
MahdiShb <mahdishb2006@gmail.com>
2024-06-24 17:41:58 +0330

    put some description on leader list pdf, made CardsPlace enum

---
+++ src/main/java/enums/cardsinformation/CardsPlace.java
@@ -0,0 +1,29 @@
+package enums.cardsinformation;
+
+import enums.Row;
+import model.Game;
+import model.card.Card;
+
+import java.util.ArrayList;
+
+public enum CardsPlace {
+    IN_GAME, IN_HAND, DECK, GRAVEYARD;
+
+    public ArrayList<Card> getCards(Game game, boolean isPayer1) {
+        if (isPayer1) {
+            return switch (this) {
+                case IN_GAME -> game.getInGameCards();
+                case IN_HAND -> game.getPlayer1InHandCards();
+                case DECK -> game.getPlayer1Deck();
+                case GRAVEYARD -> game.getPlayer1GraveyardCards();
+            };
+        } else {
+            return switch (this) {
+                case IN_GAME -> game.getInGameCards();
+                case IN_HAND -> game.getPlayer2InHandCards();
+                case DECK -> game.getPlayer2Deck();
+                case GRAVEYARD -> game.getPlayer2GraveyardCards();
+            };
+        }
+    }
+}
