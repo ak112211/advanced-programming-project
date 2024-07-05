4ee265bdb7cb40f7697926cbb5c171fda4d8efd7
ak112211 <alikhalaj53@gmail.com>
2024-07-05 18:24:18 +0330

    resolved conflicts

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,7 +17,7 @@ module Gwent {
     exports enums.cardsinformation;
     exports model.card;
     exports model.abilities;
-    opens view to javafx.fxml, com.fasterxml.jackson.databind;
+    opens view to javafx.fxml, com.fasterxml.jackson.databind, com.google.gson;
     opens model to com.google.gson, com.fasterxml.jackson.databind;
     opens enums.cardsinformation to com.google.gson, com.fasterxml.jackson.databind;
     opens model.card to com.google.gson, com.fasterxml.jackson.databind;
